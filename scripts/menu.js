c2f0cc776ae4c82e3bb14b17b3cf15e4e947fb7f
Nico <lawani.gk@gmail.com>
2023-04-14 17:02:56 +0100

    add dynamic functionality to menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,3 +4,12 @@ menuButton.onclick = () => {
   menu.classList.toggle('hidden');
   console.log('menu hidden');
 };
+const menuItems = ['Product', 'Company', 'Connect'];
+for (const item of menuItems) {
+  const arrow = document.querySelector(`#${item} .arrow`);
+  const product = document.getElementById(`${item}`);
+  product.onclick = () => {
+    arrow.classList.toggle('rotate-down');
+    console.log('Arrow flipped');
+  };
+}
