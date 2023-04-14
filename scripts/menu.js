271f914e78d537e20c14ecdd1b46e81bd5cf4f2e
Nico <lawani.gk@gmail.com>
2023-04-14 18:30:27 +0100

    complete design of mobile menu button

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,15 +1,27 @@
 const menuButton = document.querySelector('.menu-button');
+const menuIcon = menuButton.querySelector('i');
 const menu = document.querySelector('.menu');
 menuButton.onclick = () => {
   menu.classList.toggle('hidden');
   console.log('menu hidden');
+  if (menuIcon.classList.contains('fa-bars')) {
+    menuIcon.className = 'fa-solid fa-xmark x-icon';
+  } else {
+    menuIcon.className = 'fa fa-sharp fa-light fa-bars menu-icon';
+  }
 };
 const menuItems = ['Product', 'Company', 'Connect'];
 for (const item of menuItems) {
   const arrow = document.querySelector(`#${item} .arrow`);
-  const product = document.getElementById(`${item}`);
-  product.onclick = () => {
+  const menuItem = document.getElementById(`${item}`);
+  menuItem.onclick = () => {
     arrow.classList.toggle('rotate-down');
     console.log('Arrow flipped');
+    menuItem.classList.toggle('clicked');
+    const dropdown = document.querySelector(`#${item} .dropdown`);
+    console.log(dropdown);
+    if (dropdown) {
+      dropdown.classList.toggle('visible');
+    }
   };
 }
